e19f3c150a10a982d94a9a8668e318c4c6b9be36
SUITE <suite@SUITESA109-ST10.cibertec.edu.pe>
2018-09-10 21:16:34 -0500

    final

---
+++ LP_Semana_03/src/Ejercicios/Ejemplos_de_hilos_3.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class Ejemplos_de_hilos_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Nombre hilo1 = new Nombre("juan");
+		Nomber hilo2 = new Nombre("pedro");
+
+	}
+
+}
